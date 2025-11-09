36f25d49e2602bb748cb8013063249d7fa677f48
Даниил Киселев <danil@192.168.0.51>
2025-11-09 17:37:39 +0300

    init: create Angular project and modules structure

---
+++ src/app/features/movies/movies.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MoviesRoutingModule } from './movies-routing.module';
+import { MoviesComponent } from './movies.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
+
+
+@NgModule({
+  declarations: [
+    MoviesComponent,
+    MovieListComponent,
+    MovieDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    MoviesRoutingModule
+  ]
+})
+export class MoviesModule { }
