5b25cf723e940399114f1a159e392df3e3483b62
Даниил Киселев <danil@192.168.0.51>
2025-11-09 20:15:16 +0300

    feat: implement MovieService with API requests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'movies', pathMatch:'full'},
+  { path: '', redirectTo: 'movies', pathMatch: 'full' },
   { path: 'movies', loadChildren: () => import('./features/movies/movies.module').then(m => m.MoviesModule) }
 ];
 
--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable } from 'rxjs';
+import { Movie } from 'src/app/movies.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  private _baseUrl = 'http://localhost:3333/movies'
+
+  constructor(private _http: HttpClient) { }
+
+  public getMovies(): Observable<Movie[]> {
+    return this._http.get<Movie[]>(this._baseUrl).pipe(
+      catchError(err => {
+        console.error('Ошибка загрузки фильмов:', err)
+        throw err
+      })
+    )
+  }
+
+  public getMovie(id: number): Observable<Movie> {
+    return this._http.get<Movie>(`${this._baseUrl}/${id}`)
+  }
 }
